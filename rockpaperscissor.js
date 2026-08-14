a9ec07029b6efecae9275896ed39fc2de1a12e8f
Ahana Sarkar <ahanasarkar66@gmail.com>
2026-08-14 21:09:04 +0530

    Implement rock-paper-scissors game logic with computer and human choices

---
+++ rockpaperscissor.js
@@ -0,0 +1,44 @@
+
+function getComputerChoice(){
+    let compChoice= Math.floor(Math.random() * 3);
+    if (compChoice==0){
+        return "rock";
+    }
+    else if(compChoice==1){
+        return "paper";
+    }
+    else{
+        return "scissor"
+    }
+    console.log(compChoice);
+}
+
+function getHumanChoice(){
+    let playerChoice= prompt("Enter your move (rock/paper/scissor):");
+    return playerChoice.toLowerCase();
+    console.log(playerChoice);
+}
+
+function playRound(humanChoice, computerChoice) {
+  // your code here!
+    let humanScore=0;
+    let computerScore=0;
+    if (humanSelection==computerSelection){
+        console.log("Draw");
+    }
+    else if(humanSelection=="rock" && computerSelection=="paper" || humanSelection=="paper" && computerSelection=="scissor" || humanSelection=="scissor" && computerSelection=="rock"){
+        console.log("Computer wins!!");
+        computerScore++;
+        alert("Computer wins!!\nComputer score= ",computerScore," Human score= ",humanScore);
+    }
+    else{
+        onsole.log("Computer wins!!");
+        humanScore++;
+        alert("You wins!!\nComputer score= ",computerScore," Human score= ",humanScore);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
